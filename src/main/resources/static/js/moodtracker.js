b278219fce0a16bb4b347ac11d92564bfacd2607
Faisal Ahsan <f.ahsan.07@gmail.com>
2023-10-18 17:23:32 +0100

    Update time every second dynamically

--- src/main/resources/static/js/moodtracker.js
+++ src/main/resources/static/js/moodtracker.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const dateTimeElement = document.getElementById("currentDateTime");
-    const currentDateTime = new Date().toISOString();
-    dateTimeElement.innerHTML += currentDateTime;
+    const hiddenInputField = document.getElementById("clientCurrentDateTime");
 
-    // Set the hidden input field value
-    if (document.getElementById("clientCurrentDateTime")) {
-        document.getElementById("clientCurrentDateTime").value = currentDateTime;
+    function updateTime() {
+        const currentDateTime = new Date().toISOString();
+        dateTimeElement.innerHTML = 'The current date and time is: ' + currentDateTime;
+
+        // Set the hidden input field value
+        if (hiddenInputField) {
+            hiddenInputField.value = currentDateTime;
+        }
     }
+
+    // Update the time immediately upon page load
+    updateTime();
+    setInterval(updateTime, 1000);
 });
+
